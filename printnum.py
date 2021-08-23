e707e8a12c1f80656c9456b1c43daf5e9d1e2702
vimal kumar <vimal74272@gmail.com>
2021-08-23 22:04:47 +0530

    first commit

---
+++ printnum.py
@@ -0,0 +1,7 @@
+numbers = int(input("enter the n value"))
+ 
+#for looping as per python's syntax
+
+for num in range(1,numbers):
+    print (num);
+ 
---
+++ sub.py
@@ -0,0 +1,4 @@
+x=int(input("enter number"))
+y=int(input("enter number"))
+z=x-y
+print(z)
